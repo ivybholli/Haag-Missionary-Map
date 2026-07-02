4af8f4b3e1bd58958eb903c4ca3d63d8d79c689c
ivybholli <ivybholli@gmail.com>
2026-07-01 19:21:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// 1. Create the map
+const map = L.map('map').setView([20, 0], 2);
 
+// 2. Add a muted world map layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; OpenStreetMap & CartoDB'
+}).addTo(map);
