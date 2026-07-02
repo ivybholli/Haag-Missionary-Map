846107e8f6c062fe3f71e16c1b18efc1ef5589ab
ivybholli <ivybholli@gmail.com>
2026-07-01 22:53:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ console.log("🔥 APP.JS LOADED");
 const map = L.map("map").setView([20, 0], 2);
 
 L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
   {
-    attribution: "© OpenStreetMap & CartoDB"
+    attribution: "Tiles © Esri"
   }
 ).addTo(map);
 
